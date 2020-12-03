483ff10b6a951c36fd14c5763f033c5468a1684f
spaezsuarez <spaezsuarez@gmail.com>
2020-12-03 08:40:14 -0500

    Implementación inicial lista doblemente enlazada

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import models.Lista;
+import javax.swing.*;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
-       
+        Scanner entrada = new Scanner(System.in);
+        Lista l = new Lista();
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de elementos a ingresar"));
+
+        for(int i = 0; i < cantidad; i++){
+            int valor = Integer.parseInt(JOptionPane.showInputDialog("Valor: " + (i+1)));
+            l.add(valor);
+        }
+
+        l.escribir();
+
+        int remover = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el elemento a buscar"));
+        l.remove(remover);
+
+        l.escribir();
+
+        entrada.close();
+        System.exit(0);
         
     }
 }
--- src/models/Lista.java
+++ src/models/Lista.java
@@ -1,5 +1,64 @@
 package models;
 
 public class Lista {
-    
+
+    private Nodo cabeza;
+
+    public Nodo getCabeza() {
+        return cabeza;
+    }
+
+    public Lista() {
+        cabeza = null;
+    }
+
+    public void add(int value) {
+        Nodo p = new Nodo(value);
+        Nodo q = cabeza;
+        Nodo s = null;
+
+        while (q != null) {
+            s = q;
+            q = q.getNodoSiguiente();
+        }
+
+        if (s == null) {
+            cabeza = p;
+        } else {
+            s.setNodoSiguiente(p);
+            p.setNodoAnterior(s);
+        }
+    }
+
+    public void remove(int value) {
+        Nodo q = cabeza;
+        Nodo s = null, temp = null;
+
+        while (q.getValue() != value) {
+            q = q.getNodoSiguiente();
+        }
+
+        s = q.getNodoAnterior();
+        temp = q.getNodoSiguiente();
+
+        if (temp == null) {
+            s.setNodoSiguiente(null);
+        } else if (s == null) {
+            cabeza = q.getNodoSiguiente();
+        } else {
+            s.setNodoSiguiente(temp);
+            temp.setNodoAnterior(s);
+        }
+
+    }
+
+    public void escribir() {
+        Nodo iterador = cabeza;
+        while (iterador != null) {
+            System.out.print(iterador.getValue() + " ");
+            iterador = iterador.getNodoSiguiente();
+        }
+        System.out.println();
+    }
+
 }
